0e55e60ac432f172ae474f3a808febb76943c661
Harish Kumar <g.harishkumar1998@gmail.com>
2021-05-27 08:13:19 +0530

    ➕ remembering last calibration
    ➕ localstorage

--- script.js
+++ script.js
@@ -1,19 +1,27 @@
-document.getElementsByTagName('pinch-zoom')[1].style.display = "none";
-document.getElementsByTagName('pinch-zoom')[0].style.display = "";
+var btn = document.getElementById('calibrate');
+var pinchZoom1 = document.getElementsByTagName('pinch-zoom')[0];
+var pinchZoom2 = document.getElementsByTagName('pinch-zoom')[1];
 
+pinchZoom1.style.display = "";
+pinchZoom2.style.display = "none";
+
+if(localStorage.zoom) {
+    btn.innerHTML = "Re-calibrate"
+    pinchZoom1.scaleTo(localStorage.zoom, opts = {});
+}
 function toggleScale() {
     zoom = !zoom;
-    var btn = document.getElementById('calibrate');
-    btn.innerHTML = zoom ? 'OK' : 'Calibrate';
+    btn.innerHTML = zoom ? "OK" : "Re-calibrate";
     if(zoom) {
-        document.getElementsByTagName('pinch-zoom')[1].style.display = "";
-        document.getElementsByTagName('pinch-zoom')[0].style.display = "none";
+        pinchZoom1.style.display = "none"
+        pinchZoom2.style.display = "";
     }
     else {
-        document.getElementsByTagName('pinch-zoom')[1].style.display = "none";
-        document.getElementsByTagName('pinch-zoom')[0].style.display = "";
+        pinchZoom1.style.display = "";
+        pinchZoom2.style.display = "none";
         zoom = !zoom;
-        document.getElementsByTagName('pinch-zoom')[0].scaleTo(document.getElementsByTagName('pinch-zoom')[1].scale, opts = {});
+        pinchZoom1.scaleTo(pinchZoom2.scale, opts = {});
+        localStorage.setItem("zoom",pinchZoom2.scale);
         zoom = !zoom;
     }
 }
